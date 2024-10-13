32cac77a3dab5ed1cc509a25a350f4f170b31b01
misterdan100 <danielmca60@gmail.com>
2024-10-13 08:04:23 -0500

    create routes with react-router-dom 6

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,36 @@
+import {
+  Routes,
+  Route,
+  NavLink,
+  BrowserRouter
+} from 'react-router-dom';
+  
+  import logo from '../logo.svg';
+  
+  export const Navigation = () => {
+    return (
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React Logo" />
+            <ul>
+              <li>
+                <NavLink to="/" className={({isActive}) => (`${isActive ? 'nav-active' : ''}`)}>Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className={({isActive}) => (`${isActive ? 'nav-active' : ''}`)}>About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/users" className={({isActive}) => (`${isActive ? 'nav-active' : ''}`)} >Users</NavLink>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/about"  />
+            <Route path="/users" />
+            <Route path="/" />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
